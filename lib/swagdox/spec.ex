b7eea89c7d6ceebba24b0b693bc1103a833f6e0e
Dylan Blakemore <dylan.blakemore@gmail.com>
2024-03-25 12:59:10 +0200

    Move render functionality into Spec

--- lib/swagdox/spec.ex
+++ lib/swagdox/spec.ex
@@ -3,6 +3,7 @@ defmodule Swagdox.Spec do
   OpenAPI specification.
   """
   alias Swagdox.Config
+  alias Swagdox.Parameter
   alias Swagdox.Path
 
   defstruct [
@@ -52,5 +53,57 @@ defmodule Swagdox.Spec do
       schemas: [],
       securitySchemes: []
     }
+  end
+
+  @spec render(t()) :: map()
+  def render(spec) do
+    %{
+      "openapi" => spec.openapi,
+      "info" => render_info(spec.info),
+      "servers" => render_servers(spec.servers),
+      "paths" => render_paths(spec.paths),
+      "tags" => [],
+      "components" => %{}
+    }
+  end
+
+  defp render_info(info) do
+    %{
+      "title" => info.title,
+      "version" => info.version,
+      "description" => info.description
+    }
+  end
+
+  defp render_servers(servers) do
+    Enum.map(servers, fn server ->
+      %{
+        "url" => server
+      }
+    end)
+  end
+
+  defp render_paths(paths) do
+    grouped_paths = Enum.group_by(paths, & &1.path)
+
+    Enum.reduce(grouped_paths, %{}, fn {path, paths}, acc ->
+      acc_path =
+        Enum.reduce(paths, %{}, fn path, acc_path ->
+          Map.put(acc_path, path.verb, render_path(path))
+        end)
+
+      Map.put(acc, path, acc_path)
+    end)
+  end
+
+  defp render_path(path) do
+    %{
+      "description" => path.description,
+      "parameters" => render_parameters(path.parameters)
+    }
+  end
+
+  defp render_parameters(parameters) do
+    Enum.map(parameters, &Parameter.render/1)
   end
 end
--- test/swagdox/spec_test.exs
+++ test/swagdox/spec_test.exs
@@ -1,6 +1,7 @@
 defmodule Swagdox.SpecTest do
   use ExUnit.Case
 
+  alias Swagdox.Parameter
   alias Swagdox.Path
   alias Swagdox.Spec
 
@@ -41,5 +42,103 @@ defmodule Swagdox.SpecTest do
     assert %Spec{
              paths: ^paths
            } = Spec.set_paths(spec, paths)
+  end
+
+  setup do
+    paths = [
+      %Path{
+        verb: "get",
+        path: "/users",
+        description: "Returns a list of users."
+      },
+      %Path{
+        verb: "post",
+        path: "/users",
+        description: "Creates a User."
+      },
+      %Path{
+        verb: "get",
+        path: "/users/:id",
+        description: "Returns a User.",
+        parameters: [
+          %Parameter{
+            name: "id",
+            in: "query",
+            description: "User ID",
+            required: true,
+            schema: %{
+              type: "string"
+            }
+          }
+        ]
+      },
+      %Path{
+        verb: "get",
+        path: "/orders",
+        description: "Returns a list of orders."
+      }
+    ]
+
+    spec =
+      Spec.init()
+      |> Spec.set_paths(paths)
+
+    {:ok, spec: spec}
+  end
+
+  describe "render/1" do
+    test "renders the info", %{spec: spec} do
+      expected_info = %{
+        "description" => "A library for generating OpenAPI documentation from Elixir code.",
+        "title" => "Swagdox",
+        "version" => "0.1"
+      }
+
+      assert Spec.render(spec)["info"] == expected_info
+    end
+
+    test "renders the servers", %{spec: spec} do
+      expected_servers = [
+        %{
+          "url" => "http://localhost:4000"
+        }
+      ]
+
+      assert Spec.render(spec)["servers"] == expected_servers
+    end
+
+    test "renders the paths", %{spec: spec} do
+      assert %{
+               "/users" => %{
+                 "get" => %{
+                   "description" => "Returns a list of users."
+                 },
+                 "post" => %{
+                   "description" => "Creates a User."
+                 }
+               },
+               "/users/:id" => %{
+                 "get" => %{
+                   "description" => "Returns a User.",
+                   "parameters" => [
+                     %{
+                       "description" => "User ID",
+                       "in" => "query",
+                       "name" => "id",
+                       "required" => true,
+                       "schema" => %{
+                         "type" => "string"
+                       }
+                     }
+                   ]
+                 }
+               },
+               "/orders" => %{
+                 "get" => %{
+                   "description" => "Returns a list of orders."
+                 }
+               }
+             } = Spec.render(spec)["paths"]
+    end
   end
 end
